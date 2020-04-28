b1bbfb77d276380be3e18cf9ef1db0644220a3f0
Tongfu <861454910@qq.com>
2020-04-29 01:17:45 +0800

    Initial commit

---
+++ lib/second.dart
@@ -0,0 +1,39 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MySecondPage extends StatefulWidget{
+  final String title = "子页面";
+  MySecondPage(){
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    return MySecondPageState();
+  }
+
+}
+
+class MySecondPageState extends State<MySecondPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text("这是第二个页面"),
+            RawMaterialButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      ),
+
+    );
+  }
+
+}
